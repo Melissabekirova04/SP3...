e258835af725c1ddbde0a9d7925bea3c79d88214
MortenHelander <morten.helander@hotmail.com>
2025-11-06 14:22:03 +0100

    FileIO og TextUI

--- src/Media.java
+++ src/Media.java
@@ -1,2 +1,16 @@
-public class Media {
+public abstract class Media {
+    private String title;
+    private int releaseDate;
+    private double rating;
+    private String category;
+    private int duration;
+
+    public Media(String title, int releaseDate, double rating, String category, int duration) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+        this.category = category;
+    }
+
+
 }
--- src/Movies.java
+++ src/Movies.java
@@ -1,2 +1,8 @@
-public class Movies {
+public class Movies extends Media{
+
+    public Movies(String title, int releaseDate, double rating, String category, int duration) {
+        super(title, releaseDate, rating, category, duration);
+    }
+
+
 }
